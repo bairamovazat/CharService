8f9db13937966bcbfa410bd94bb126dd03141478
azat <bairamovazat@mail.com>
2018-04-21 17:02:06 +0300

    delete all files

--- src/main/java/ru/ivmiit/service/RegistrationService.java
+++
@@ -1,9 +0,0 @@
-package ru.ivmiit.service;
-
-import ru.ivmiit.models.User;
-
-public interface RegistrationService {
-    void registerUser(User user) throws IllegalArgumentException;
-
-    boolean userIsRegistered(String userName);
-}
